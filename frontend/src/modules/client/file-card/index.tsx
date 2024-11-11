744e65421c8ebeb41b1660453acd00712a53e5b7
madzalo <madzaloresten8@protonmail.com>
2024-11-11 14:49:06 +0200

    view documents online

--- frontend/src/modules/client/file-card/index.tsx
+++ frontend/src/modules/client/file-card/index.tsx
@@ -1,9 +1,8 @@
 import { Card } from '@/components/ui/card';
-import {
-  convertFileSize
-} from '@/lib/utils';
+import { convertFileSize } from '@/lib/utils';
 import DownloadButton from '@/modules/common/download-button';
 import Thumbnail from '@/modules/common/thumbnail';
+import Link from 'next/link';
 
 export interface FileCardProps {
   file: {
@@ -21,41 +20,43 @@ export interface FileCardProps {
 
 const FileCard = ({ file }: FileCardProps) => {
   return (
-    <Card className='file-card'>
-      <div className='flex justify-between'>
-        <Thumbnail
-          type={file.type}
-          extension={file.extension}
-          url={file.url}
-          className='!size-20'
-          imageClassName='!size-11'
-        />
-
-        <div className='flex flex-col items-end justify-between'>
-          <DownloadButton
-            fileName={file.name}
-            fileExtension={file.extension}
+    <Link href={file.url} target='_blank'>
+      <Card className='file-card'>
+        <div className='flex justify-between'>
+          <Thumbnail
+            type={file.type}
+            extension={file.extension}
+            url={file.url}
+            className='!size-20'
+            imageClassName='!size-11'
           />
-          {/* <ActionDropdown file={file} /> */}
-          <p className='body-1'>
-            {convertFileSize(file.size)}
-          </p>
-        </div>
-      </div>
 
-      <div className='file-card-details'>
-        <p className='subtitle-2 line-clamp-1'>
-          {file.name}
-        </p>
-        {/* <FormattedDateTime
+          <div className='flex flex-col items-end justify-between'>
+            <DownloadButton
+              fileName={file.name}
+              fileExtension={file.extension}
+            />
+            {/* <ActionDropdown file={file} /> */}
+            <p className='body-1'>
+              {convertFileSize(file.size)}
+            </p>
+          </div>
+        </div>
+
+        <div className='file-card-details'>
+          <p className='subtitle-2 line-clamp-1'>
+            {file.name}
+          </p>
+          {/* <FormattedDateTime
           date={file.$createdAt}
           className='body-2 text-light-100'
         /> */}
-        {/* <p className='caption line-clamp-1 text-light-200'>
+          {/* <p className='caption line-clamp-1 text-light-200'>
           By: {file.owner.fullName}
         </p> */}
-      </div>
-    </Card>
+        </div>
+      </Card>
+    </Link>
   );
 };
 export default FileCard;
