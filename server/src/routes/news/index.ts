eaa9145c541944e763810b3a0e6e252a24facf4c
madzalo <madzaloresten8@protonmail.com>
2024-11-15 10:32:09 +0200

    get news by id

--- server/src/routes/news/index.ts
+++ server/src/routes/news/index.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
 import {
   createNews,
-  deleteNews,
   getAllNews,
   getNewsById,
-  updateNews,
 } from '../../controllers/news';
 import { verifyToken } from '../../middlewares/verify-token/index';
 
@@ -13,7 +11,5 @@ const router = Router();
 router.post('/', verifyToken, createNews);
 router.get('/', getAllNews);
 router.get('/:id', getNewsById);
-router.patch('/:id', verifyToken, updateNews);
-router.delete('/:id', verifyToken, deleteNews);
 
 export default router;
