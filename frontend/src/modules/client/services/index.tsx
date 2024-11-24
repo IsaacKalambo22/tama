1dfe71c77f98aa06a4c8fce820ed0b2ef7eb0faa
madzalo <madzaloresten8@protonmail.com>
2024-11-24 15:40:04 +0200

    custom services page

--- frontend/src/modules/client/services/index.tsx
+++ frontend/src/modules/client/services/index.tsx
@@ -1,53 +1,14 @@
 import HeaderText from '@/modules/common/header-text';
-import ServiceCard from './service-card';
+import CustomService from '../home/custom-service';
 
 const Services = () => {
   return (
-    <div className='flex flex-col items-center gap-16 mb-16'>
+    <div className='flex flex-col items-center gap-10 mb-16'>
       <HeaderText
         title='Our Services'
         subtitle='Empowering farmers with resources and support'
       />
-      <div className='grid grid-cols-1 sm:grid-cols-2 w-full  gap-12 '>
-        <ServiceCard
-          title='Satellite depot operations'
-          points={[
-            'Receiving centers of Tobacco haulage to Floors Markets',
-            'Act as information centers to farmers in rural areas',
-            'Outlets of service deliveries like Hessian distribution.',
-            'Receiving centers of Tobacco haulage to Floors Markets',
-            'Act as information centers to farmers in rural areas',
-            'Outlets of service deliveries like Hessian distribution.',
-          ]}
-        />
-        <ServiceCard
-          title='Satellite depot operations'
-          points={[
-            'Receiving centers of Tobacco haulage to Floors Markets',
-            'Act as information centers to farmers in rural areas',
-            'Outlets of service deliveries like Hessian distribution.',
-            'Receiving centers of Tobacco haulage to Floors Markets',
-            'Act as information centers to farmers in rural areas',
-            'Outlets of service deliveries like Hessian distribution.',
-          ]}
-        />
-        <ServiceCard
-          title='Satellite depot operations'
-          points={[
-            'Receiving centers of Tobacco haulage to Floors Markets',
-            'Act as information centers to farmers in rural areas',
-            'Outlets of service deliveries like Hessian distribution.',
-          ]}
-        />
-        <ServiceCard
-          title='Hessian Services'
-          points={[
-            'Offers a subsidized scheme to its members',
-            "Collects Hessians from buyers on growers's behalf",
-            'Replenishes ex-rated pieces and also buys for the scheme new pieces to meet growing numbers of voluntarily registered members',
-          ]}
-        />
-      </div>
+      <CustomService />
     </div>
   );
 };
