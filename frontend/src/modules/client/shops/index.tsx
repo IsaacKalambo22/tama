fbb860dfb6aad72903765d528d560e4beabd7a84
madzalo <madzaloresten8@protonmail.com>
2024-11-25 10:55:05 +0200

    get client shops from API

--- frontend/src/modules/client/shops/index.tsx
+++ frontend/src/modules/client/shops/index.tsx
@@ -1,94 +1,98 @@
+import { fetchShops } from '@/lib/api';
+import { BASE_URL } from '@/lib/utils';
 import HeaderText from '@/modules/common/header-text';
 import ShopCard from './shop-card';
 import ShopItemCard from './shop-item-card';
 
-const Shops = () => {
+const Shops = async () => {
+  let shops = [];
+  try {
+    shops = await fetchShops(); // Fetch the data directly
+  } catch (error) {
+    console.error(
+      'Failed to fetch shops:',
+      error
+    );
+    return (
+      <div className='flex flex-col w-full max-w-7xl mx-auto items-center gap-16 mb-16'>
+        <HeaderText
+          title='Discover Our Locations'
+          subtitle='Find Our Trusted Stores Near You'
+        />
+
+        <p className='text-red-500'>
+          Failed to load shops and documents.
+        </p>
+      </div>
+    );
+  }
+
   return (
-    <div className='flex flex-col items-center gap-12 mb-16'>
+    <div className='flex flex-col w-full max-w-7xl mx-auto gap-16 mb-16 px-4 sm:px-6 lg:px-8'>
       <HeaderText
         title='Discover Our Locations'
         subtitle='Find Our Trusted Stores Near You'
       />
 
-      {/* Shop Locations */}
-      <div className='grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'>
-        <ShopCard
-          imageUrl='/assets/images/shop1.jpg'
-          name='Kasiya Shop'
-          address='123 Main St, City'
-          openingHours='Open: Mon-Sat 9am - 6pm'
+      {/* Render the files */}
+      <>
+        {shops.length > 0 ? (
+          <div className='file-list w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+            {shops.map((shop) => (
+              <ShopCard
+                key={shop.id}
+                imageUrl={`${BASE_URL}/uploads/${shop.imageUrl}`}
+                name={shop.name}
+                address={shop.address}
+                openingHours={shop.openHours}
+              />
+            ))}
+          </div>
+        ) : (
+          <p className='empty-list'>
+            No Reports and Publications
+          </p>
+        )}
+        {/* Items for Sale */}
+        <HeaderText
+          title='Available Items'
+          subtitle='Browse Items at Our Shops'
         />
-        <ShopCard
-          imageUrl='/assets/images/shop1.jpg'
-          name='Market Plaza'
-          address='456 Center Rd, City'
-          openingHours='Open: Mon-Fri 10am - 5pm'
-        />
-        <ShopCard
-          imageUrl='/assets/images/shop1.jpg'
-          name='Market Plaza'
-          address='456 Center Rd, City'
-          openingHours='Open: Mon-Fri 10am - 5pm'
-        />
-        <ShopCard
-          imageUrl='/assets/images/shop1.jpg'
-          name='Market Plaza'
-          address='456 Center Rd, City'
-          openingHours='Open: Mon-Fri 10am - 5pm'
-        />
-        <ShopCard
-          imageUrl='/assets/images/shop1.jpg'
-          name='Market Plaza'
-          address='456 Center Rd, City'
-          openingHours='Open: Mon-Fri 10am - 5pm'
-        />
-        <ShopCard
-          imageUrl='/assets/images/shop1.jpg'
-          name='Market Plaza'
-          address='456 Center Rd, City'
-          openingHours='Open: Mon-Fri 10am - 5pm'
-        />
-      </div>
-
-      {/* Items for Sale */}
-      <HeaderText
-        title='Available Items'
-        subtitle='Browse Items at Our Shops'
-      />
-      <div className='grid gap-8 grid-cols-2 sm:grid-cols-3 lg:grid-cols-4'>
-        <ShopItemCard
-          imageUrl='/assets/images/shop1.jpg'
-          name='Syngenta'
-        />
-        <ShopItemCard
-          imageUrl='/assets/images/shop1.jpg'
-          name='Syngenta'
-        />
-        <ShopItemCard
-          imageUrl='/assets/images/shop1.jpg'
-          name='Syngenta'
-        />
-        <ShopItemCard
-          imageUrl='/assets/images/shop1.jpg'
-          name='Syngenta'
-        />
-        <ShopItemCard
-          imageUrl='/assets/images/shop1.jpg'
-          name='Syngenta'
-        />
-        <ShopItemCard
-          imageUrl='/assets/images/shop1.jpg'
-          name='Syngenta'
-        />
-        <ShopItemCard
-          imageUrl='/assets/images/shop1.jpg'
-          name='Syngenta'
-        />
-        <ShopItemCard
-          imageUrl='/assets/images/shop1.jpg'
-          name='Syngenta'
-        />
-      </div>
+        <div className='grid gap-8 grid-cols-2 sm:grid-cols-3 lg:grid-cols-4'>
+          <ShopItemCard
+            imageUrl='/assets/images/shop1.jpg'
+            name='Syngenta'
+          />
+          <ShopItemCard
+            imageUrl='/assets/images/shop1.jpg'
+            name='Syngenta'
+          />
+          <ShopItemCard
+            imageUrl='/assets/images/shop1.jpg'
+            name='Syngenta'
+          />
+          <ShopItemCard
+            imageUrl='/assets/images/shop1.jpg'
+            name='Syngenta'
+          />
+          <ShopItemCard
+            imageUrl='/assets/images/shop1.jpg'
+            name='Syngenta'
+          />
+          <ShopItemCard
+            imageUrl='/assets/images/shop1.jpg'
+            name='Syngenta'
+          />
+          <ShopItemCard
+            imageUrl='/assets/images/shop1.jpg'
+            name='Syngenta'
+          />
+          <ShopItemCard
+            imageUrl='/assets/images/shop1.jpg'
+            name='Syngenta'
+          />
+        </div>
+      </>
     </div>
   );
 };
