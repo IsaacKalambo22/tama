71110ad13fde68218bd1ebf014f0be35960f2cca
madzalo <madzaloresten8@protonmail.com>
2024-12-12 06:45:56 +0200

    auth secret

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -3,6 +3,7 @@ import Credentials from 'next-auth/providers/credentials';
 
 export const { handlers, auth, signIn, signOut } =
   NextAuth({
+    secret: process.env.AUTH_SECRET,
     providers: [
       Credentials({
         credentials: {
