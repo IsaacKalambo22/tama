1cc345abe611d5268df70b7325b5beee24523e5f
madzalo <madzaloresten8@protonmail.com>
2025-01-07 08:50:03 +0200

    sending emails from TAMA

--- server/src/mailtrap/config/index.ts
+++ server/src/mailtrap/config/index.ts
@@ -8,6 +8,6 @@ export const mailtrapClient = new MailtrapClient({
 });
 
 export const sender = {
-  email: 'hello@demomailtrap.com',
+  email: 'hello@tamalawi.com',
   name: 'TAMA Farmers Trust',
 };
