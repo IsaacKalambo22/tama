1972ca503e73971c1283e592d763e66db7d98c10
madzalo <madzaloresten8@protonmail.com>
2025-05-16 12:42:45 +0200

    updated middleware

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -12,97 +12,90 @@ export const userRoutes = "/user"
 export const adminRoutes = "/admin"
 
 export const middleware = auth(async (req) => {
-  try {
-    const secretKey = process.env.AUTH_SECRET
+  const secretKey = process.env.AUTH_SECRET
 
-    if (!secretKey) {
-      console.error("AUTH_SECRET is not defined")
-      // Return next response instead of throwing error
-      return NextResponse.next()
-    }
+  if (!secretKey) {
+    throw new Error("AUTH_SECRET is not defined")
+  }
 
-    const token = req.auth
+  const token = req.auth
 
-    const { nextUrl } = req
-    const isLoggedIn = !!token
-    console.log({ isLoggedIn })
+  const { nextUrl } = req
+  const isLoggedIn = !!token
+  console.log({ isLoggedIn })
 
-    // Route checks
-    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
-    const isAuthRoute =
-      authRoutes.includes(nextUrl.pathname) ||
-      nextUrl.pathname.startsWith("/set-password") ||
-      nextUrl.pathname.startsWith("/reset-password")
+  // Route checks
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+  const isAuthRoute =
+    authRoutes.includes(nextUrl.pathname) ||
+    nextUrl.pathname.startsWith("/set-password") ||
+    nextUrl.pathname.startsWith("/reset-password")
 
-    const isAdminRoute = nextUrl.pathname.startsWith(adminRoutes)
-    const isPublicRoute =
-      publicRoutes.includes(nextUrl.pathname) ||
-      nextUrl.pathname.startsWith("/tobacco-business") ||
-      nextUrl.pathname.startsWith("/news-updates") ||
-      nextUrl.pathname.startsWith("/resources") ||
-      nextUrl.pathname.startsWith("/api")
+  const isAdminRoute = nextUrl.pathname.startsWith(adminRoutes)
+  const isPublicRoute =
+    publicRoutes.includes(nextUrl.pathname) ||
+    nextUrl.pathname.startsWith("/tobacco-business") ||
+    nextUrl.pathname.startsWith("/news-updates") ||
+    nextUrl.pathname.startsWith("/resources") ||
+    nextUrl.pathname.startsWith("/api")
 
-    // Skip API auth routes
-    if (isApiAuthRoute) return NextResponse.next()
-    if (isPublicRoute) return NextResponse.next()
+  // Skip API auth routes
+  if (isApiAuthRoute) return
+  if (isPublicRoute) return
 
-    // Allow public access to the login page without redirecting if not logged in
-    if (isAuthRoute && !isLoggedIn) return NextResponse.next()
+  // Allow public access to the login page without redirecting if not logged in
+  if (isAuthRoute && !isLoggedIn) return
 
-    // Redirect logged-in users away from sign-in/up pages
-    if (isAuthRoute && isLoggedIn) {
-      const redirectTo =
-        token.role === "ADMIN" || token.role === "MANAGER" ? "/admin" : "/user"
-      return NextResponse.redirect(new URL(redirectTo, nextUrl))
-    }
+  // Redirect logged-in users away from sign-in/up pages
+  if (isAuthRoute && isLoggedIn) {
+    const redirectTo =
+      token.role === "ADMIN" || token.role === "MANAGER" ? "/admin" : "/user"
+    return NextResponse.redirect(new URL(redirectTo, nextUrl))
+  }
 
-    // Redirect to login if not logged in and accessing a protected route
-    if (!isLoggedIn && !isPublicRoute) {
+  // Redirect to login if not logged in and accessing a protected route
+  if (!isLoggedIn && !isPublicRoute) {
+    return NextResponse.redirect(new URL("/sign-in", nextUrl))
+  }
+
+  // Restrict access to admin routes
+  if (isAdminRoute) {
+    if (!isLoggedIn) {
       return NextResponse.redirect(new URL("/sign-in", nextUrl))
     }
 
-    // Restrict access to admin routes
-    if (isAdminRoute) {
-      if (!isLoggedIn) {
-        return NextResponse.redirect(new URL("/sign-in", nextUrl))
-      }
-
-      if (token.role !== "ADMIN" && token.role !== "MANAGER") {
-        return NextResponse.redirect(new URL("/current-dashboard", nextUrl)) // Prevent non-admin users from accessing
-      }
-
-      return NextResponse.next()
+    if (token.role !== "ADMIN" && token.role !== "MANAGER") {
+      return NextResponse.redirect(new URL("/current-dashboard", nextUrl)) // Prevent non-admin users from accessing
     }
 
-    // Role-based redirection if logged in and accessing the wrong route
-    if (isLoggedIn && token) {
-      switch (token?.role) {
-        case "USER":
-          if (!nextUrl.pathname.startsWith("/user")) {
-            return NextResponse.redirect(new URL("/user", nextUrl))
-          }
-          break
-
-        case "ADMIN":
-        case "MANAGER":
-          if (!nextUrl.pathname.startsWith("/admin")) {
-            return NextResponse.redirect(new URL("/admin", nextUrl))
-          }
-          break
-
-        default:
-          if (nextUrl.pathname !== DEFAULT_LOGIN_REDIRECT) {
-            return NextResponse.redirect(new URL("/", nextUrl))
-          }
-      }
-    }
-
-    // No redirect needed
-    return NextResponse.next()
-  } catch (error) {
-    console.error("Middleware error:", error)
-    return NextResponse.next()
+    return
   }
+
+  // Role-based redirection if logged in and accessing the wrong route
+  if (isLoggedIn && token) {
+    switch (token?.role) {
+      case "USER":
+        if (!nextUrl.pathname.startsWith("/user")) {
+          return NextResponse.redirect(new URL("/user", nextUrl))
+        }
+        break
+
+      case "ADMIN":
+      case "MANAGER":
+        if (!nextUrl.pathname.startsWith("/admin")) {
+          return NextResponse.redirect(new URL("/admin", nextUrl))
+        }
+        break
+
+      default:
+        if (nextUrl.pathname !== DEFAULT_LOGIN_REDIRECT) {
+          return NextResponse.redirect(new URL("/", nextUrl))
+        }
+    }
+  }
+
+  // No redirect needed
+  return
 })
 
 // Configure the middleware to match specific routes
