025572d712ec2956c6c6d2ce48fd1a226ce5bdd6
GoldenKingOwen <matolaowen1@gmail.com>
2026-08-27 16:05:51 +0200

    refactor(api): use transactional email service in auth

--- apps/api/src/controllers/auth/index.ts
+++ apps/api/src/controllers/auth/index.ts
@@ -4,13 +4,13 @@ import { Request, Response } from "express"
 
 import { Role } from "../../../prisma/generated/prisma"
 import prisma from "../../config"
+import { notifyEvent } from "../../messaging/transactional-email"
 import {
   sendPasswordResetEmail,
   sendResetSuccessEmail,
   sendSetPasswordSuccessEmail,
   setPasswordRequestEmail,
 } from "../../nodemailer/emails"
-import { notifyEvent } from "../../notifications/service"
 import { canAssignRole, ScopeUser } from "../../permissions"
 import { APIResponse } from "../../types"
 import { generateTokens } from "../../utils/generate-tokens"
